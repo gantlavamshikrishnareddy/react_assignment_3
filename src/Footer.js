31622a26d500d1634692b6aa224352bf9b424db7
gantlavamshikrishnareddy <38282111+gantlavamshikrishnareddy@users.noreply.github.com>
2022-04-08 03:02:57 -0500

    Add files via upload

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="bg-light"></footer>
+      <p className="text-center"> © 2021 Technical. All Rights Reserved | Terms and Conditions apply Attribution-ShareAlike 4.0 International<a href="https://creativecommons.org/licenses/by-sa/4.0/">(CC BY-SA 4.0)</a> </p>
+    </>
+  );
+};
+
+export default Footer;
---
+++ src/Sdata.js
@@ -0,0 +1,35 @@
+import c from "../src/images/1.jpeg";
+import java from "../src/images/2.png";
+import php from "../src/images/3.jpeg";
+import python from "../src/images/4.jpeg";
+import andriod from "../src/images/5.jpeg";
+import web from "../src/images/6.jpeg";
+
+const Sdata = [
+    {
+        imgsrc: c,
+        title: "Jenna L. Gorlewicz, Ph.D.",
+    },
+    {
+        imgsrc: java,
+        title:"Jen L. Tennison, Ph.D.",
+    },
+    {
+        imgsrc:php,
+        title:"Maya Dunlap Ph.D. Student",
+    },
+    {
+        imgsrc:python,
+        title: "Scott Lambert Ph.D. Student",
+    },
+    {
+        imgsrc:andriod,
+        title: "Bryan MacGavin Ph.D. Student",
+    },
+    {
+        imgsrc:web,
+        title:"Josh Adams M.S. Student",
+    },
+];
+
+export default Sdata;
